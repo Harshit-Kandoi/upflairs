70d6211089f2021d5a42acaad5fdc29f0efb8538
Harshit Kandoi <kandoiharshitsdlp@gmail.com>
2024-06-30 22:20:27 +0530

    hey

--- hello_world.py
+++ hello_world.py
@@ -4,7 +4,7 @@ print(st[17:24])
 print(st[24:33])
 print(st[-1:-9]) """
 
-st = "upflairs"
-print(st[::])
-print(st[:])
-print(st[::3])
+# st = "upflairs"
+# print(st[::])
+# print(st[:])
+# print(st[::3])
